5b08903b6a0a456511db592a3f8249fcb862151c
Dev shah <devshah@Devs-MacBook-Air.local>
2025-04-16 01:23:52 +0530

    first commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
+import pymongo
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
 
 app = Flask(__name__)
@@ -22,64 +23,72 @@ def index():
             flipkartPage = uClient.read()
             uClient.close()
             flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
+
+            bigboxes = flipkart_html.findAll("div", {"class": "cPHDOP col-12-12"})
             del bigboxes[0:3]
+            if not bigboxes:
+                return "Flipkart page layout has changed. No products found."
+
             box = bigboxes[0]
             productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
             prodRes = requests.get(productLink)
             prodRes.encoding='utf-8'
             prod_html = bs(prodRes.text, "html.parser")
-            print(prod_html)
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
-
+            commentboxes = prod_html.find_all('div', {'class': "RcXBOT"})
             filename = searchString + ".csv"
             fw = open(filename, "w")
             headers = "Product, Customer Name, Rating, Heading, Comment \n"
             fw.write(headers)
             reviews = []
+
             for commentbox in commentboxes:
                 try:
-                    #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
-
+                    name = commentbox.div.div.find_all('p', {'class': '_2NsDsF AwS1CA'})[0].text
                 except:
+                    name = 'No Name'
                     logging.info("name")
 
                 try:
-                    #rating.encode(encoding='utf-8')
                     rating = commentbox.div.div.div.div.text
-
-
                 except:
                     rating = 'No Rating'
                     logging.info("rating")
 
                 try:
-                    #commentHead.encode(encoding='utf-8')
                     commentHead = commentbox.div.div.div.p.text
-
                 except:
                     commentHead = 'No Comment Heading'
                     logging.info(commentHead)
+
                 try:
                     comtag = commentbox.div.div.find_all('div', {'class': ''})
-                    #custComment.encode(encoding='utf-8')
                     custComment = comtag[0].div.text
                 except Exception as e:
+                    custComment = 'No Comment'
                     logging.info(e)
 
-                mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
-                          "Comment": custComment}
+                mydict = {
+                    "Product": searchString,
+                    "Name": name,
+                    "Rating": rating,
+                    "CommentHead": commentHead,
+                    "Comment": custComment
+                }
                 reviews.append(mydict)
+
             logging.info("log my final result {}".format(reviews))
-            return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
+            client = pymongo.MongoClient("mongodb://devshah0408:Dev1234Shah@ac-spsojno-shard-00-00.fhwfzdr.mongodb.net:27017,ac-spsojno-shard-00-01.fhwfzdr.mongodb.net:27017,ac-spsojno-shard-00-02.fhwfzdr.mongodb.net:27017/?replicaSet=atlas-xsyp0u-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=Cluster0")
+            db = client['review_scrap']
+            review_col = db['review_scrap_data']
+            review_col.insert_many(reviews)
+            
+            return render_template('result.html', reviews=reviews)
         except Exception as e:
             logging.info(e)
-            return 'something is wrong'
-    # return render_template('results.html')
-
+            return f'Something is wrong: {e}'
     else:
         return render_template('index.html')
+
 
 
 if __name__=="__main__":
